b59e37db9eb7d37ca7b5848e6cb10298c0d4c354
Julian Hidalgo <julian.hidalgo.leon28@gmail.com>
2024-05-27 21:03:46 -0600

    [General][Course advancement]

--- src/main_class.java
+++ src/main_class.java
@@ -1,6 +1,25 @@
 public class main_class {
-    //Main method
+    // Main method
     public static void main(String [] args){
+        // Class instance
+        model_movie movie = new model_movie();
 
+        movie.name = "The Lord of the Rings";
+        movie.releaseDate = 2001;
+        movie.duration = 178;
+
+        System.out.println("My movie is: " + movie.name);
+        System.out.println("Release date: " + movie.releaseDate);
+        System.out.println("Duration: " + movie.duration);
+
+        // Create another movie
+        model_movie second_movie = new model_movie();
+        second_movie.name = "Shrek";
+        second_movie.releaseDate = 2001;
+        second_movie.duration = 92;
+
+        System.out.println("My second movie is: " + second_movie.name);
+        System.out.println("Release date: " + second_movie.releaseDate);
+        System.out.println("Duration" + second_movie.duration);
     }
 }
