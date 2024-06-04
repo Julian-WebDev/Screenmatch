887ce23be023bf5b88d101cd1763712d192913f4
Julian Hidalgo <julian.hidalgo.leon28@gmail.com>
2024-06-03 20:58:12 -0600

    [General][OOP Course]

--- src/main_class.java
+++ src/main_class.java
@@ -8,18 +8,29 @@ public class main_class {
         movie.releaseDate = 2001;
         movie.duration = 178;
 
-        System.out.println("My movie is: " + movie.name);
-        System.out.println("Release date: " + movie.releaseDate);
-        System.out.println("Duration: " + movie.duration);
-
+        movie.showMovie();
+        movie.rating(8);
+        movie.rating(10);
+        movie.rating(9);
         // Create another movie
         model_movie second_movie = new model_movie();
+
         second_movie.name = "Shrek";
         second_movie.releaseDate = 2001;
         second_movie.duration = 92;
+        System.out.println(movie.rating);
 
-        System.out.println("My second movie is: " + second_movie.name);
-        System.out.println("Release date: " + second_movie.releaseDate);
-        System.out.println("Duration" + second_movie.duration);
+        System.out.println("Average: " + movie.average());
+
+        //System.out.println("My second movie is: " + second_movie.name);
+        //System.out.println("Release date: " + second_movie.releaseDate);
+        //System.out.println("Duration" + second_movie.duration);
+
+        second_movie.showMovie();
+        second_movie.rating(8);
+        second_movie.rating(7);
+        second_movie.rating(6);
+        System.out.println(second_movie.rating);
+
     }
 }
--- src/model_movie.java
+++ src/model_movie.java
@@ -5,6 +5,23 @@ public class model_movie {
     int duration;
     boolean available;
 
+    double rating;
+    int totalRatings;
+
+    void showMovie(){
+        System.out.println("The name of the movie is: " + name);
+        System.out.println("Release date: " + releaseDate);
+        System.out.println("Duration: " + duration);
+    }
 
 
+    void rating(double rate){
+        rating += rate;
+        rating = rating + rate;
+        totalRatings ++;
+    }
+
+    double average(){
+        return rating / totalRatings;
+    }
 }
